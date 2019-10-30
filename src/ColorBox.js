2a2f322b934702100aae77363ea39231daafcb85
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-10-30 06:15:57 -0500

    removed a trailing space in the ColorBox.css file name.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './ColorBox.css';
 
 class ColorBox extends Component {
 
     render() {
         return (
-            <div>
+            <div className='ColorBox'>
                 <h1> This is a box. </h1>
             </div>
         )
