c2adc27c141e36569a935950beb3000fa1815d0e
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-10-30 06:06:01 -0500

    initial enviorment set up. started first component for the color boxes

---
+++ src/ColorBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ColorBox extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> This is a box. </h1>
+            </div>
+        )
+    }
+}
+
+export default ColorBox; 
